c444269392bf68f9185fed787fb401e12c02ceeb
Raffaele135 <mariano.raffaele@liceoviconapoli.it>
2020-12-08 18:14:23 +0100

    Update matploit_1.py

--- matploit_1.py
+++ matploit_1.py
@@ -8,7 +8,7 @@ fig, axs = plt.subplots(1, 3, figsize=(9, 3), sharey=True)
 axs[0].bar(names, values)
 axs[1].scatter(names, values)
 axs[2].plot(names, values)
-fig.suptitle('Categorical Plotting')
+fig.suptitle('Quale è la stagione preferita dai ragazzi?')
 
 
 plt.show()
