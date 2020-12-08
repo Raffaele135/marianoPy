d6ecbd7a41b2674cc5a834e52680c87646786e2d
Raffaele135 <mariano.raffaele@liceoviconapoli.it>
2020-12-08 18:07:59 +0100

    Create esercizio_stringhe.py

---
+++ esercizio_stringhe.py
@@ -0,0 +1,5 @@
+stringa = list(input("La parola è "))
+alfabeto = ("abcdefghijklmnopqrstuvwxyz")
+for lettera in alfabeto:
+    if stringa.count(lettera):
+        print(lettera , "=" , stringa.count(lettera))
