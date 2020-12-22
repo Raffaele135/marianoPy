a5c8b63e40aa1348c138a95e680999c7ffaa2a0e
Raffaele135 <mariano.raffaele@liceoviconapoli.it>
2020-12-22 21:50:26 +0100

    Update matploit.py

--- matploit.py
+++ matploit.py
@@ -1,7 +1,7 @@
 import matplotlib
 import matplotlib.pyplot as plt 
 import numpy as np 
-x = ([1,5,10,2,4,5])
-y = ([5,10,15,25,2])
+x = ([5,10,2,4,5])
+y = ([1,10,15,25,2])
 plt.plot(x, y)
 plt.show()
