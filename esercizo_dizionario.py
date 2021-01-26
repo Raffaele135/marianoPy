a51c2a329673327e887e38fedb4623e103b09733
Raffaele135 <mariano.raffaele@liceoviconapoli.it>
2021-01-26 16:54:42 +0100

    Delete esercizo_dizionario.py

--- esercizo_dizionario.py
+++
@@ -1,6 +0,0 @@
-stringa = dict(input("La parola è "))
-
-alfabeto = ("abcdefghijklmnopqrstuvwxyz")
-for lettera in alfabeto:
-    if stringa.count(lettera):
-        print(lettera , "=" , stringa.count(lettera))
