07153f35b0630995f412b1be4eab050bae98aeb1
Raffaele135 <mariano.raffaele@liceoviconapoli.it>
2021-05-11 09:55:38 +0200

    Update software_haiku_1.py

--- haiku/software_haiku_1.py
+++ haiku/software_haiku_1.py
@@ -1,8 +1,19 @@
 #software realizzato da Mariano e di Gennaro per il gruppo Mariano, di Gennaro, Greco, Iannuzzi0
+from csv import reader
+
+with open('versi.csv', 'r') as csv_file:
+    csv_reader = reader(csv_file)
+    list_of_column = list(csv_reader)
+nuova_lista_1 = []
+nuova_lista_2 = []
+nuova_lista_3 = []
+
+for i in list_of_column:
+    nuova_lista_1.append(i[0])
+    nuova_lista_2.append(i[1])
+    nuova_lista_3.append(i[2])
+
 import random 
-lista_a = ["elefante", "maiale", "pollo"]
-lista_b = ["bello", "brutto", "medio"]
-lista_c = ["quaderno", "matita", "penna"]
  
 def estrazione(lista): 
     p=random.randint(0,len(lista)-1) 
@@ -10,9 +21,9 @@ def estrazione(lista):
     lista.remove(x) 
     return x 
   
-x=estrazione(lista_a) 
-y=estrazione(lista_b)
-z=estrazione(lista_c)
+x=estrazione(nuova_lista_1) 
+y=estrazione(nuova_lista_2)
+z=estrazione(nuova_lista_3)
 
 print (x)
 print (y)
