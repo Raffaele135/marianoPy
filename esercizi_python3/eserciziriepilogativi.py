fcc77c7d177aa1b00e8f2e47ec2420d404f961cb
Raffaele135 <mariano.raffaele@liceoviconapoli.it>
2021-05-11 09:06:11 +0200

    Update eserciziriepilogativi.py

--- esercizi_python3/eserciziriepilogativi.py
+++ esercizi_python3/eserciziriepilogativi.py
@@ -1,6 +1,8 @@
+#Calcola la media dei voti 
+from numpy import mean
 voti = {
-    "Mario": {"matematica": "6", "italiano": "6", "scienze": "7", "inglese": "4"}    
-    "Giovanni": {"matematica": "4", "italiano": "6", "scienze": "5", "inglese": "7"}
+    "Mario": {"matematica": "6", "italiano": "6", "scienze": "7", "inglese": "4"},    
+    "Giovanni": {"matematica": "4", "italiano": "6", "scienze": "5", "inglese": "7"},
     "Paola": {"matematica": "9", "italiano": "6", "scienze": "8", "inglese": "8"}
 }
 
