b666b28380d204e5b670fc77c2d8737b1ce12201
Raffaele135 <mariano.raffaele@liceoviconapoli.it>
2021-10-11 08:13:21 +0200

    Update retta_1.py

--- coniche/retta_1.py
+++ coniche/retta_1.py
@@ -2,13 +2,12 @@ print("Lavorare con equazione scritta nella forma: ax + by + c = 0")
 class retta:
     
 
-    def __init__(self, a, b, c, x):
+    def __init__(self, a, b, c):
         self.__a = float(a)
         self.__b = float(b)
         self.__c = float(c)
-        self.__x = float(x)
         self.__punti = []
-        
+
 
     def Implicita(self):
         if self.__b == 0:
@@ -36,29 +35,43 @@ class retta:
         else:
             return f"\nCoefficiente angolare: \n m = {-self.__b / self.__a}"
     
-    def trovaY(self):
-        y = (-self.__a * self.__x) / self.__b + (-self.__c / self.__b)
-        return f"\n Y: \n y = {-self.__a * self.__x / self.__b + (-self.__c / self.__b)}"
+    def trovaY(self, x):
+        return f"\n Y: \n y = {-self.__a * x / self.__b + (-self.__c / self.__b)}"
 
 
     def punti(self, N, M, x):
-        self.__N = N
-        self.__M = M
+        self.N = N
+        self.M = M
     
-        for self.__N in range (self.__M):
+        for self.N in range (self.M):
             tupla = (x, (-self.__a * x) / self.__b + (-self.__c / self.__b))
             x = x + 1
             self.__punti.append(tupla)
         return f"\n Le coordinate dei punti appartenenti alla retta sono: \n {self.__punti}"         
 
 
+    def instersezione(self, a1 , b1 , c1):
+        self.__a1 = float(a1)
+        self.__b1 = float(b1)
+        self.__c1 = float(c1)
+        if (-self.__b / self.__a) == (-self.__b1 / self.__a1):
+            if self.__c == self.__c1:
+                return f"\nLe rette sono coincidenti \n {self.__punti}"
+            else:
+                return f"Null"
+        elif self.__c == self.__c1:
+            return f"\nIl putnto di incontro delle due rette è: (0, {self.__c})" 
+        else:
+            return f"\nLe rette sono incidenti e la coordinata del punto d'incidenza è: ({((-self.__c / self.__b)+(self.__c1 / self.__b1))/((-self.__b / self.__a)+(self.__b1 / self.__a1))}, {((-self.__b / self.__c)+(self.__b1 / self.__c1))/((-self.__b / self.__a)+(self.__b1 / self.__a1))})"
 
-valori = retta(4, 2, 1, 3)
+valori = retta(2, 3, 4)
 print(valori.Implicita())
 print(valori.Esplicita())
 print(valori.m())
-print(valori.trovaY())
-print(valori.punti(0, 300, -100))
+print(valori.trovaY(2))
+print(valori.punti(0, 10, 5))
+print(valori.instersezione(2, 3, 5))
+
 
 
 
