a0184567569bbc077874630a83d4d1a64fc4b002
Raffaele135 <mariano.raffaele@liceoviconapoli.it>
2021-11-07 18:02:39 +0100

    Revert "Update retta_1.py"
    This reverts commit 5aa5067f43b6b00a578b275c7a12832351663eaa.

--- coniche/retta_1.py
+++ coniche/retta_1.py
@@ -33,20 +33,19 @@ class retta:
         if self.__b == 0:
             return f"\nCoefficiente angolare: \n Il coefficiente angolare non è definito; la retta è parallela all'asse y"
         else:
-            return f"\nCoefficiente angolare: \n m = {-self.__a / self.__b}"
+            return f"\nCoefficiente angolare: \n m = {-self.__b / self.__a}"
     
     def trovaY(self, x):
-        self.__x = float(x)
-        return f"\n Y: \n y = {-self.__a * self.__x / self.__b + (-self.__c / self.__b)}"
+        return f"\n Y: \n y = {-self.__a * x / self.__b + (-self.__c / self.__b)}"
 
 
-    def punti(self, N, M):
-        self.N = int(N)
-        self.M = int(M)
+    def punti(self, N, M, x):
+        self.N = N
+        self.M = M
     
         for self.N in range (self.M):
-            tupla = (self.__x, (-self.__a * self.__x) / self.__b + (-self.__c / self.__b))
-            self.__x = self.__x + 1
+            tupla = (x, (-self.__a * x) / self.__b + (-self.__c / self.__b))
+            x = x + 1
             self.__punti.append(tupla)
         return f"\n Le coordinate dei punti appartenenti alla retta sono: \n {self.__punti}"         
 
@@ -59,24 +58,20 @@ class retta:
             if self.__c == self.__c1:
                 return f"\nLe rette sono coincidenti \n {self.__punti}"
             else:
-                return f"\nNull"
+                return f"Null"
         elif self.__c == self.__c1:
             return f"\nIl putnto di incontro delle due rette è: (0, {self.__c})" 
         else:
             return f"\nLe rette sono incidenti e la coordinata del punto d'incidenza è: ({((-self.__c / self.__b)+(self.__c1 / self.__b1))/((-self.__b / self.__a)+(self.__b1 / self.__a1))}, {((-self.__b / self.__c)+(self.__b1 / self.__c1))/((-self.__b / self.__a)+(self.__b1 / self.__a1))})"
 
-    def fascio_parallelo(self):
-        if self.__b == 0:
-            return f"\nL'equazione del fascio parallelo alla retta data è: \nx = k"
-        else:
-            return f"\nL'equazione del fascio parallelo alla retta data è: \ny = {-self.__a / self.__b}x + q"
-
-
-valori = retta(input('a = ' ), input('b = ' ), input('c = ' ))
+valori = retta(2, 3, 4)
 print(valori.Implicita())
 print(valori.Esplicita())
 print(valori.m())
-print(valori.trovaY(input('x = ')))
-print(valori.punti(input('inizio intervallo = ') , input('fine intervallo = ')))
-print(valori.instersezione(input('a1 = ' ), input('b1 = ' ), input('c1 = ' )))
-print(valori.fascio_parallelo())
+print(valori.trovaY(2))
+print(valori.punti(0, 10, 5))
+print(valori.instersezione(2, 3, 5))
+
+
+
+
